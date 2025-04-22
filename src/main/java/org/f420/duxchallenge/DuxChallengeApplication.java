68fff8d7b6fc411f39cd4ef8142b122255b618b0
Federico Silva <fedes7777@gmail.com>
2025-04-21 22:54:55 -0300

    Initial commit

---
+++ src/main/java/org/f420/duxchallenge/DuxChallengeApplication.java
@@ -0,0 +1,13 @@
+package org.f420.duxchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuxChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DuxChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/f420/duxchallenge/DuxChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.f420.duxchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuxChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
